9a97473d65efb8531309e7fe9ba153d2edb2f697
chaihahaha <836275709@qq.com>
2018-09-22 10:49:26 +0800

    WSL

---
+++ WSL-debian/install.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt full-upgrade
+sudo apt install git man sqlite3 openssh-client gdb binutils jed jed-extra haskell-platform build-essential npm docker.io texlive texlive-xetex texlive-lang-chinese texlive-lang-english ffmpeg aubio-tools
+bash /mnt/d/linuxopen/linuxOpen/Anaconda3-4.2.0-Linux-x86_64.sh
+ln -s /mnt/d/ubuntu/ ~/win
+conda update --prefix /mnt/d/ubuntu/anaconda anaconda
+pip install keyrings.alt jrnl
+
