b6fb823280837c195c2f5a51c06b91441edc55bd
chaihahaha <chai@DESKTOP-3S92ON3.localdomain>
2019-01-10 23:33:48 +0800

    2019.1.10

--- WSL-debian/install.sh
+++ WSL-debian/install.sh
@@ -3,6 +3,8 @@ sudo apt update
 sudo apt full-upgrade
 sudo apt install git man sqlite3 openssh-client gdb binutils jed jed-extra haskell-platform build-essential npm docker.io ffmpeg aubio-tools lua5.3
 #bash /mnt/d/linuxopen/linuxOpen/Anaconda3-4.2.0-Linux-x86_64.sh
+git config --global user.mail "836275709@qq.com"
+git config --global user.name "chaihahaha"
 conda update --prefix /mnt/d/ubuntu/anaconda anaconda
 #pip install keyrings.alt jrnl
 curl -fsSL get.docker.com -o get-docker.sh
