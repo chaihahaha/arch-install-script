1fceb043ab56048918eb8c513d4a5571872bb06f
chaihahaha <chai@DESKTOP-3S92ON3.localdomain>
2019-01-10 23:33:15 +0800

    2019.1.10

--- WSL-debian/install.sh
+++ WSL-debian/install.sh
@@ -1,9 +1,9 @@
+#ln -s /mnt/d/ubuntu/ ~/win
 sudo apt update
 sudo apt full-upgrade
-sudo apt install git man sqlite3 openssh-client gdb binutils jed jed-extra haskell-platform build-essential npm docker.io texlive texlive-xetex texlive-lang-chinese texlive-lang-english ffmpeg aubio-tools lua5.3
-bash /mnt/d/linuxopen/linuxOpen/Anaconda3-4.2.0-Linux-x86_64.sh
-ln -s /mnt/d/ubuntu/ ~/win
+sudo apt install git man sqlite3 openssh-client gdb binutils jed jed-extra haskell-platform build-essential npm docker.io ffmpeg aubio-tools lua5.3
+#bash /mnt/d/linuxopen/linuxOpen/Anaconda3-4.2.0-Linux-x86_64.sh
 conda update --prefix /mnt/d/ubuntu/anaconda anaconda
-pip install keyrings.alt jrnl
+#pip install keyrings.alt jrnl
 curl -fsSL get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
