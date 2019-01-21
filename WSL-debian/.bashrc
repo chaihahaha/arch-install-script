5838559212a956b99601ac676bc8c03fbfc5fcf6
chaihahaha <chai@DESKTOP-TD015H4.localdomain>
2019-01-21 17:03:43 +0800

    ^M

--- WSL-debian/.bashrc
+++ WSL-debian/.bashrc
@@ -115,4 +115,4 @@ if ! shopt -oq posix; then
     . /etc/bash_completion
   fi
 fi
-export PATH="/mnt/d/ubuntu/anaconda/bin:$PATH"
+export PATH="/mnt/e/ubuntu/anaconda/bin:$PATH"
--- WSL-debian/install.sh
+++ WSL-debian/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-ln -s /mnt/d/ubuntu/ ~/win
+ln -s /mnt/e/ubuntu/ ~/win
 cp ~/win/.bashrc ~/
 sudo cp ~/win/sources.list /etc/apt/
 source ~/.bashrc
 sudo apt update
 sudo apt full-upgrade
-sudo apt install git man sqlite3 openssh-client gdb binutils jed jed-extra haskell-platform build-essential ffmpeg aubio-tools lua5.3
-#bash /mnt/d/linuxopen/linuxOpen/Anaconda3-4.2.0-Linux-x86_64.sh
+sudo apt install telnet git man sqlite3 openssh-client gdb binutils jed jed-extra haskell-platform build-essential ffmpeg aubio-tools lua5.3
+#bash /mnt/e/linuxopen/linuxOpen/Anaconda3-4.2.0-Linux-x86_64.sh
 git config --global user.mail "836275709@qq.com"
 git config --global user.name "chaihahaha"
 conda update --prefix /mnt/d/ubuntu/anaconda anaconda
